5ebf201ab074ac888ccbf95b5c7809951d98c033
dragadan <seif.hammami20@gmail.com>
2014-05-12 20:20:08 +0100

    commit

---
+++ hr_extension/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "hr_extension",
+    "version": "1.0",
+    "depends": ["base","hr"],
+    "author": "dragadan",
+    "category": "update",
+    "description": """
+    This module provide :
+    """,
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': ['hr_extension_view.xml'],
+    'installable': True,
+    'active': False,
+}
---
+++ hr_extension/hr_extension.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import openerp
+from openerp import netsvc, tools, pooler
+from openerp.osv import fields, osv
+
+
+class hr_extension(osv.osv):
+    
+    _name='hr.employee'
+    
+    _inherit='hr.employee'
+    
+    _columns = {
+            'zied':fields.char('New', size=64, required=True),
+                    }
+    
+hr_extension()    
